0b8c829842a5a3c50ab31e78ad4f47c046780de4
Shivani Gupta <shivanigupta103131@gmail.com>
2023-08-07 21:48:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Card } from "@mui/material";
+import Card from "./components/Card"
 import Header from "./components/Header";
 
 function App() {
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='flex flex-wrap justify-between p-3 mt-2'>
+<div className='card shadow-lg font-medium p-2 hover:-translate-y-2 cursor-pointer '>
+
+  {/* <img className='h-40' src = "#" alt ="image"/> */}
+  <h1>Name: lorem ipsum</h1>
+  <h1>Rating: 5</h1>
+  <h1>Year:2005</h1>
+</div>
+
+    </div>
   )
 }
 
