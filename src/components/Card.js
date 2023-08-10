2268e2a29871c4d07cf408e480cd4827626f9bbc
Shivani Gupta <shivanigupta103131@gmail.com>
2023-08-10 19:43:25 +0530

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import ReactStars from 'react-stars';
 import {ThreeDots} from "react-loader-spinner";
+import {getDocs} from 'firebase/firestore';
+import { moviesRef } from '../firebase/firebase';
+
 const Card = () => {
   const [data, setdata] = useState([]);
-const [loading, setloading] = useState(true);
+const [loading, setloading] = useState(false);
   useEffect(() =>{
     async function getData()
     {
-
+setloading(true);
+const _data = await getDocs(moviesRef);
+_data.forEach((doc)=>{
+  setdata((prev)=> [...prev, doc.data()])
+})
+setloading(false);
     }
     getData();
   },[])
@@ -17,8 +25,8 @@ const [loading, setloading] = useState(true);
       {loading ? <div className='w-full flex justify-center items-center h-96'> <ThreeDots height ={40} color="white"/></div> :
 data.map((e, i)=>{
   return(
-    <div key = {i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer'>
-<img className='h-72' src = {e.image} />
+    <div key = {i} className='card font-medium shadow-lg p-2 hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500'>
+<img className='h-72' src = {e.image} alt = "dummyimage" />
 <h1>
   <span className='text-gray-500'>Name:</span>{e.title}
 </h1>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import React from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import {Button} from '@mui/material';
 import {Link} from 'react-router-dom';
+
 const Header = () => {
   return (
     <div className='sticky top-0 header z-10 text-3xl flex justify-between text-red-500 p-3 font-bold border-b-2 border-gray-300'>
-
+<Link to ={'/'}>
     <span>Filmy
-   <span className='text-white'>Verse</span></span>
+   <span className='text-white'>Verse</span></span></Link>
    <Link to ={'/addmovie'}>
    <h1 className='text-lg flex items-center cursor-pointer'>
     <Button><AddIcon className='mr-1' color='secondary'/><span className='text-white'>Add New</span></Button>
